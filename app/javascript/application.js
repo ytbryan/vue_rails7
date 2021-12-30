8a3cd8c72c6293c18ef8dc590150770255a27105
ytbryan <ytbryan@gmail.com>
2021-12-30 21:44:50 +0800

    Turbo.session.drive = false will fix vue component not loading.
    Switching off Turbo drive is one way to make Vue work in Rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
+
 import "controllers"
 import "vue"
 import "./parts/first"
+
+
