dab3aedebe2967082ced938fb969def94d8c6aa4
ytbryan <ytbryan@gmail.com>
2021-12-30 21:45:46 +0800

    addEventListener("turbo:load" to fix Vue
    Surrounding the Vue instance in a turbo:load event is another way to make Vue works in Turbo-ed Rails

--- app/javascript/parts/first.js
+++ app/javascript/parts/first.js
@@ -1,11 +1,14 @@
+document.addEventListener("turbo:load", function() {
 var app5 = new Vue({
     el: '#app-5',
     data: {
-      message: 'Hello Vue.js!'
+        message: 'Hello Vue.js!'
     },
     methods: {
-      reverseMessage: function () {
+        reverseMessage: function () {
         this.message = this.message.split('').reverse().join('')
-      }
+        }
     }
-  })
+    })
+})
+  
